35a171d20865eeab32e6308e94c0060e5dcf6c39
Lee Ching <qaz741qhn@gmail.com>
2021-08-14 15:52:19 +0900

    permit additional parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, only: [:update]
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 
 end
